696cea635b64c07f436e2d553708b86edff232f6
development0261 <development0261@gmail.com>
2021-08-02 17:56:31 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
-import * as express from "express";
+var http = require('http');
+const url = require('url');
+const dotenv = require('dotenv');
+const path = require('path');
+const Joi = require('joi');
 
-const app = express();
-app.use(express.json());
-const methodRouter = require('./methodRouter.route');
-
-// register routes
-app.use('/', methodRouter);
-
-//port
-app.listen(3000);
+dotenv.config({ path: path.join(__dirname, '../.env') });
+const envVarsSchema = Joi.object();
+const { value: envVars } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+const {
+    PostMethod,
+    GetMethod,
+    DeleteMethod,
+    PatchMethod
+} = require('./controller');
+const server = http.createServer(async (req, res) => {
+    if (req.url.match(/\/user?/) && req.method === "GET") {
+        GetMethod(req, res)
+    }
+    else if (req.url === "/user" && req.method === "GET") {
+        GetMethod(req, res)
+    }
+    else if (req.url === "/user" && req.method === "POST") {
+        PostMethod(req, res)
+    }
+    else if(req.url.match(/\/user?/) && req.method === "PATCH") {
+        PatchMethod(req, res)
+    }
+    else if(req.url.match(/\/user?/) && req.method === "DELETE") {
+        DeleteMethod(req, res)
+    }
+})
+server.listen(envVars.PORT, () => {
+    console.log(`server started on port: 3000`);
+});
